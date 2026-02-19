1b6a41c0cb46dcbf763feba253ff17ef9bdc5ba1
StephenOkpeku <Stephen@appentus.in>
2026-02-19 11:03:47 +0100

    update

--- config/cors.ts
+++ config/cors.ts
@@ -44,7 +44,7 @@ const corsConfig: CorsConfig = {
   |                     one of the above values.
   |
   */
-  origin: "*",
+  origin: true,
   // origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
 
   /*
